f36116b550380996863406e05eccb339ddd2b808
hoviet <47932884+hoviet@users.noreply.github.com>
2019-02-25 15:45:27 +0700

    Add files via upload

---
+++ WS/WebApp/WebApp/Models/HomePage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class HomePage
+    {
+        public int id { get; set; }
+        public String tenDanhMuc { get; set; }
+        public List<SanPham> sanPham { get; set; }
+    }
+}
