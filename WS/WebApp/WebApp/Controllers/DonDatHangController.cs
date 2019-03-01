ee628cb7475b0dd2a0fd480c97a5ee151973a82c
hoviet <47932884+hoviet@users.noreply.github.com>
2019-03-01 17:24:13 +0700

    Update DonDatHangController.cs

--- WS/WebApp/WebApp/Controllers/DonDatHangController.cs
+++ WS/WebApp/WebApp/Controllers/DonDatHangController.cs
@@ -61,6 +61,97 @@ namespace WebApp.Controllers
                 return BadRequest(ex.Message);
             }
         }
+         //hien thi mot hoa don dat hang chi tiet
+        [HttpGet]
+        [ActionName("hoaDon")]
+        public IHttpActionResult getHoaDon(int idDonHang, int idKhachHang) //id
+        {
+            try
+            {
+                HienThiDonHang ctDonHang = new HienThiDonHang();
+                DonDatHang donHang = db.DonDatHangs.FirstOrDefault(x => x.id_don_hang == idDonHang && x.id_khach_hang == idKhachHang);
+                if(donHang == null)
+                {
+                    return NotFound();
+                }
+                ctDonHang.idDonDatHang = donHang.id_don_hang;
+                ctDonHang.trangThai = db.TinhTrangDonHangs.FirstOrDefault(x => x.id_tinh_trang == donHang.id_tinh_trang).tinh_trang_don_hang;
+                ctDonHang.ngayLap = donHang.ngay_lap;
+                ctDonHang.tenNguoiNhan = db.KhachHangs.FirstOrDefault(x => x.id_khach_hang == donHang.id_khach_hang).ten_nguoi_dung;
+                ctDonHang.soDT ="0"+ donHang.so_dt_nguoi_nhan;
+                ctDonHang.diaChi = donHang.noi_nhan;
+                // add danh sach san pham
+                List<ChiTietDonHang> list = db.ChiTietDonHangs.Where(x => x.id_don_hang == idDonHang).ToList();
+                List<DSSanPham> lDanhSanPham = new List<DSSanPham>();
+                for(int i = 0; i< list.Count; i++)
+                {
+                    DSSanPham dsp = new DSSanPham();                   
+                    dsp.soLuong = list[i].so_luong;
+                    dsp.tongGia =(float) list[i].tong_tien;
+                    dsp.giaKM = (float)list[i].gia_km;
+                    //gan san pham 
+                    SanPham tam = db.SanPhams.FirstOrDefault(x => x.id_san_pham == list[i].id_san_pham);
+                    SanPham sp = new SanPham();
+                    sp.id_san_pham = tam.id_san_pham;
+                    sp.id_danh_muc = tam.id_danh_muc;
+                    sp.mo_ta = tam.mo_ta;
+                    sp.phan_tram_km = tam.phan_tram_km;
+                    sp.ten_sp = tam.ten_sp;
+                    sp.url_hinh_chinh = tam.url_hinh_chinh;
+                    sp.gia_sp = tam.gia_sp;
+                    sp.gia_km = tam.gia_km;
+                    dsp.sanPhan = sp;
+                    lDanhSanPham.Add(dsp);
+                }
+                ctDonHang.danhSachHang = lDanhSanPham;
+                return Ok(ctDonHang);
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        //tao don dat hang moi va luu cao xsdl
+        [HttpPost]
+        [ActionName("themHoaDon")]
+        public IHttpActionResult themHoaDon([FromBody] TaoDonHang donHang)
+        {
+            try
+            {
+                //tao 1 don dat hang
+                DonDatHang ddh = new DonDatHang();
+                ddh.id_khach_hang = donHang.idKhachHang;
+                ddh.id_tinh_trang = donHang.idTinhTrang;
+                ddh.ngay_lap = donHang.ngayLap;
+                ddh.tong_tien = donHang.tongTien;
+                ddh.so_dt_nguoi_nhan = donHang.soDT;
+                ddh.noi_nhan = donHang.noiNhan;
+                ddh.ghi_chu = donHang.ghiChu;
+                db.DonDatHangs.InsertOnSubmit(ddh);
+                db.SubmitChanges();
+                //tao chi tiet don dat hang tuong ung
+                DonDatHang tam = db.DonDatHangs.Where(x => x.id_khach_hang == donHang.idKhachHang).ToList().Last();
+
+                for(int i = 0; i <donHang.danhSachSanPham.Count; i++)
+                {
+                    ChiTietDonHang ctDonHang = new ChiTietDonHang();
+                    int id = donHang.danhSachSanPham[i].idSanPhan;
+                    ctDonHang.id_don_hang = tam.id_don_hang;
+                    ctDonHang.id_san_pham = 1;
+                    ctDonHang.gia_km = donHang.danhSachSanPham[i].giaKM;
+                    ctDonHang.so_luong = donHang.danhSachSanPham[i].soLuong;
+                    ctDonHang.tong_tien = donHang.danhSachSanPham[i].tongGia;
+                    ctDonHang.thoi_gian_lap = donHang.ngayLap;
+
+                    db.ChiTietDonHangs.InsertOnSubmit(ctDonHang);
+                    db.SubmitChanges();
+                }
+                
+                return Ok(donHang);
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
         [HttpPost]
         [ActionName("insert")]
         public IHttpActionResult insertDonDatHang([FromBody] DonDatHang donDatHang)
