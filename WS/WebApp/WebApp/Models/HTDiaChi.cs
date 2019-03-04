953ed21fcd9267a16b1f6b3aa9eb51592a7d1b50
hoviet <47932884+hoviet@users.noreply.github.com>
2019-03-04 15:45:45 +0700

    Add files via upload

---
+++ WS/WebApp/WebApp/Models/HTDiaChi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class HTDiaChi
+    {
+        public int idDiaChiKhachHang { get; set; }
+        public string tenDiaChi { get; set; }
+        public string tenKhachHang { get; set; }
+        public string soDt { get; set; }
+        public int idKhachHang { get; set; }
+        public bool loai { get; set; }
+    }
+}
