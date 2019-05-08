3f44093605739161bef077bd305455f760708743
hoviet <47932884+hoviet@users.noreply.github.com>
2019-05-08 19:41:20 +0700

    5/8/2019

--- WS/WebApp/WebApp/Controllers/AdminController.cs
+++ WS/WebApp/WebApp/Controllers/AdminController.cs
@@ -4,10 +4,35 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class AdminController : ApiController
     {
+        QuanLyBanHangDataContext db = new QuanLyBanHangDataContext();
+
+        [HttpGet]
+        [ActionName("DangNhap")]
+        public IHttpActionResult dangNhap([FromBody] DN admin)
+        {
+            try
+            {
+                Admin ad = db.Admins.FirstOrDefault(e => e.user.Equals(admin.user) && e.pass.Equals(admin.pass));
+                if(ad== null)
+                {
+                    return StatusCode(HttpStatusCode.NoContent);
+                }
+                var tam = new
+                {
+                    user = ad.user,
+                    id = ad.id,
+                };
+                return Ok();
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
